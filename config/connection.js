10e0fcb58200642af2c6e446c4eb830a2a08691c
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-01-22 09:39:42 -0600

    Connect node to mysql

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'burger_db',
+});
+
+db.connect(function(err) {
+    if(err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log("Connected as id " + db.threadId);
+});
+
+module.exports = db;
+
