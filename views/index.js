291b063bb655822e6bdb4f58d3a3d2848ed0dd37
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-01-26 12:27:58 -0600

    Create server side html rendering

---
+++ views/index.js
@@ -0,0 +1,35 @@
+
+const render = function ({id, burger, devour}) {
+    return `
+    <div>
+        <text>EAT - DA - BURGER!!!</text>
+    </div>
+
+    /* TODO: add the loop here */
+    <div class="form-group">
+        <ul>
+            <li>
+                <p> <span class"label"> </span> ${id}</p>
+                <p> <span class="label"> </span> ${burger}</p>
+                
+                <button class="devour">Devour it</button>
+            </li>
+        </ul>
+    </div>
+
+<div id=center> 
+    <form id="form"> 
+        <input type="text" id="burgerName" rows="4" cols="50"> </textarea>
+                <button type="button" id="submitBtn" onclick="myFunction()">Submit</button>
+                <p> Enter a burger you want to eat Click the "Submit" button</p>
+    </form>
+</div>
+
+<div id= right>
+    /* TODO : add loop */
+    <p>${id} . ${burger}<p>
+</div>
+`
+}
+
+exports.render = render;
