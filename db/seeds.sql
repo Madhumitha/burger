49b26376b331e7e9f066ea19b58c52d198fe20cc
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-01-25 20:55:03 -0600

    Fix a bug

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,2 @@
 INSERT INTO burgers (burger_name, devoured)
-VALUES("Veggie burger", true), ("Teriyaki burger", true), ("UnCUT plant-based burger", true);
+VALUES("Veggie burger", 0), ("Teriyaki burger", 0), ("UnCUT plant-based burger", 0);
