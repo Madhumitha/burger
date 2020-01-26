ce7e2e1878e709ed5ef870c0da04b5c571fa105d
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-01-26 12:35:25 -0600

    Include connections and orm

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var db = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: 'root',
     database: 'burger_db',
 });
 
--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,29 @@
 var connection = require("./connection.js");
 
+// Object for all our SQL statement functions
+
 var orm = {
-    selectAll: function(burger_db) {
+    selectAll: function(burger_db, cb) {
         var queryString = "SELECT * FROM ??";
         connection.query(queryString, burger_db, function(err, res){
             if(err) throw err;
-            console.log(res);
+            cb(res);
         })
     },
-    insertOne: function(burger_db, burger_name, devoured) {
+    insertOne: function(burger_db, burger_name, devoured,cb) {
         var queryString = "INSERT INTO ?? (??, ??)";
         connection.query(queryString, [burger_db, burger_name, devoured], function(err, res){
             if(err) throw err;
             console.log(res);
+            cb(res);
         })
     },
-    updateOne: function(burger_db, burger_name, devoured) {
+    updateOne: function(burger_db, burger_name, devoured, cb) {
         var queryString = "UPDATE ?? SET ?? = ??";
         connection.query(queryString, [burger_db, burger_name, devoured], function(err, res){
             if(err) throw err;
             console.log(res);
+            cb(res);
         })
     }
 }
