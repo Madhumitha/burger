55a56052a4cf283b2284d5ffc7430ad0d01efdd5
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-01-26 12:35:59 -0600

    Improvise database code

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE burger_db;
 
 CREATE TABLE burgers (
     id INTEGER NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(100),
-    devoured TINYINT(1),
+    burger_name VARCHAR(100) NOT NULL,
+    devoured TINYINT(1) NOT NULL,
     PRIMARY KEY(id)
 );
