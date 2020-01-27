ccc84ceb5fa3bb0d6395d0ce7218a7df8887e536
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-01-27 16:36:24 -0600

    Fix abstracted layer of ORM

--- models/burger.js
+++ models/burger.js
@@ -3,18 +3,18 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function(cb) {
-        orm.all('burgers', function(res) {
+    selectAll: function(cb) {
+        orm.selectAll('burgers', function(res) {
             cb(res);
         });
     },
-    insert: function(burger_name, devoured, cb) {
-        orm.insert("burgers", burger_name, devoured, function(res){
+    insertOne: function(burger_name, devoured, cb) {
+        orm.insertOne("burgers", burger_name, devoured, function(res){
             cb(res);
         });
     },
-    update: function(burger_name, devoured, cb) {
-        orm.update("burgers", burger_name, devoured, function(res){
+    updateOne: function(burger_name, devoured, cb) {
+        orm.updateOne("burgers", burger_name, devoured, function(res){
             cb(res);
         });
     }
