835a1a10f7569535c922ccdf0ed49b75f62a628f
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-01-28 11:55:37 -0600

    Add indent in controller folder

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -22,7 +22,11 @@ router.get('/', function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    burger.insertOne([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
         res.json({id: result.insertId});
     });
 });
@@ -32,13 +36,10 @@ router.put("/api/burgers/:id", function(req, res) {
   
     console.log("condition", condition);
   
-    burger.updateOne(
-      {
+    burger.updateOne({
         devoured: req.body.devoured
-      },
-      condition,
-      function(result) {
-        if (result.changedRows === 0) {
+      }, condition, function(result) {
+        if (result.changedRows == 0) {
           // If no rows were changed, then the ID must not exist, so 404
           return res.status(404).end();
         }
