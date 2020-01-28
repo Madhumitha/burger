1220d13a3e617557df8f06f7754c91415c7e66c8
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-01-28 11:21:47 -0600

    Import views and modify routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,6 +5,11 @@ var router = express.Router();
 // Import the model burger.js to use its database functions
 var burger = require('../models/burger.js');
 
+// Import the views to complete MVC workflow
+
+const mainLayoutView = require('./views/layouts/main.js');
+const indexLayoutView = require('./views/index.js');
+
 // Create all our routes and set up logic within those routes where required.
 router.get('/', function(req, res) {
     burger.selectAll(function(data) {
@@ -12,12 +17,35 @@ router.get('/', function(req, res) {
             burgers: data
         };
         console.log(hbsObject);
-        res.render("index", hbsObject)
+        res.send(mainLayoutView.render(indexLayoutView.render(hbsObject)));
     });
 });
 
-// TODO: Still need to create routes for insert and update
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+        res.json({id: result.insertId});
+    });
+});
 
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.updateOne(
+      {
+        devoured: req.body.devoured
+      },
+      condition,
+      function(result) {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+      });
+  });
+  
   
   // Export routes for server.js to use.
   module.exports = router;
